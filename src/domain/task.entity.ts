6f6a432c6a1a6dbaeeb1e912c22283d8c0ca80d2
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-18 22:20:16 +0200

    feat: Update Task entity with EntityId for id property

--- src/domain/task.entity.ts
+++ src/domain/task.entity.ts
@@ -1,6 +1,8 @@
+import { EntityId } from "src/libs/entity-id.vo";
+
 export class Task {
     constructor(
-        private readonly id: string,
+        private readonly id: EntityId,
         private title: string,
         private description: string,
         private isCompleted: boolean,
