401fc37e011f59047da80c41d7672afb769ec3ef
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-19 14:03:55 +0200

    refactor: Update CreateTaskHandler to use Title value object for task name

--- src/application/create-task.handler.ts
+++ src/application/create-task.handler.ts
@@ -2,11 +2,12 @@ import { TaskRepository } from "src/domain/task.repository";
 import { CreateTaskCommand } from "./create-task.command";
 import { Task } from "src/domain/task.entity";
 import { EntityId } from "src/libs/entity-id.vo";
+import { Title } from "src/domain/title.vo";
 
 export class CreateTaskHandler {
     constructor(private readonly taskRepository: TaskRepository) { }
     async execute(command: CreateTaskCommand): Promise<void> {
-        const task = new Task(new EntityId(), command.name, command.description, false);
+        const task = new Task(new EntityId(), new Title(command.name), command.description, false);
         await this.taskRepository.save(task);
     }
 }
