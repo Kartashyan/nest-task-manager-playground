c6ec3a84716214661c00f50c3e546cda53198335
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-19 14:03:49 +0200

    refactor: Update Title value object to validate title length

--- src/domain/title.vo.ts
+++ src/domain/title.vo.ts
@@ -1,7 +1,15 @@
 import { ValueObject, ValueObjectProps } from "src/libs/value-object.abstract";
 
 export class Title extends ValueObject<string> {
-    protected validate(props: ValueObjectProps<string>): boolean {
-        return props.value.length > 0;
+    constructor(title: string) {
+        super({ value: title });
+    }
+    protected validate(title: ValueObjectProps<string>): void {
+        if (title.value.length < 1) {
+            throw new Error('Title must have at least 1 character');
+        }
+    }
+    get value(): string {
+        return this.props.value;
     }
 }
