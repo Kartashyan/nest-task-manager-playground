376575a8d8d4d5ee74c01dc3667edd9f6b63f94a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-19 13:03:07 +0200

    refactor: Refactor ValueObject interface to use a generic abstract class

--- src/libs/value-object.interface.ts
+++ src/libs/value-object.interface.ts
@@ -1,4 +1,29 @@
-export interface ValueObject<T> {
-    equals(valueObject: ValueObject<T>): boolean;
+type Primitive = string | number | boolean;
+
+type SingleValue<T extends Primitive | Date> = {
     value: T;
+};
+
+type ValueObjectProps<T> = T extends Primitive | Date ? SingleValue<T> : T;
+
+
+export abstract class ValueObject<T> {
+    protected readonly props: ValueObjectProps<T>;
+
+    constructor(props: ValueObjectProps<T>) {
+        Object.freeze(this);
+        this.validate(props);
+        this.props = props;
+    }
+
+    equals(valueObject: ValueObject<T>): boolean {
+        if (valueObject === null || valueObject === undefined) {
+            return false;
+        }
+        if (valueObject.props === undefined) {
+            return false;
+        }
+        return this.props === valueObject.props;
+    }
+    protected abstract validate(props: ValueObjectProps<T>): boolean;
 }
