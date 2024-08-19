ae4cec4496a06278309830e150cdca6be8ac3e18
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-19 16:43:47 +0200

    refactor: Update Task entity constructor to accept TaskProps

--- src/domain/task.entity.ts
+++ src/domain/task.entity.ts
@@ -1,13 +1,26 @@
 import { EntityId } from "src/libs/entity-id.vo";
 import { Title } from "./title.vo";
 
+export type TaskProps = {
+    id: EntityId;
+    title: Title;
+    description: string;
+    isCompleted: boolean;
+};
+
 export class Task {
-    constructor(
-        private readonly id: EntityId,
-        private title: Title,
-        private description: string,
-        private isCompleted: boolean,
-    ) { }
+    private readonly id: EntityId;
+    private title: Title;
+    private description: string;
+    private isCompleted: boolean;
+
+    constructor(props: TaskProps) {
+        this.id = props.id;
+        this.title = props.title;
+        this.description = props.description;
+        this.isCompleted = props.isCompleted;
+    }
+
     completeTask() {
         this.isCompleted = true;
     }
