208ebf613c6194d6abf87c34ea6627e5f5401df9
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-19 14:03:44 +0200

    feat: Update Task entity to use Title value object for representing a title

--- src/domain/task.entity.ts
+++ src/domain/task.entity.ts
@@ -1,9 +1,10 @@
 import { EntityId } from "src/libs/entity-id.vo";
+import { Title } from "./title.vo";
 
 export class Task {
     constructor(
         private readonly id: EntityId,
-        private title: string,
+        private title: Title,
         private description: string,
         private isCompleted: boolean,
     ) { }
@@ -12,5 +13,17 @@ export class Task {
     }
     getId() {
         return this.id;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getIsCompleted() {
+        return this.isCompleted;
     }
 }
