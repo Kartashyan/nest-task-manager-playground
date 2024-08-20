148c3da3b14b0895b0180ea4697ba819f408bc48
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-20 15:57:43 +0200

    refactor: Add CqrsModule to Task module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [TaskController],
   providers: [TaskService],
 })
