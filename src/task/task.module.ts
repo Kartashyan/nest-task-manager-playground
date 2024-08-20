5fa5d7311f0e64f8ed91e61b3d6bac8e395ed838
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-20 21:11:38 +0200

    refactor: Update TaskModule imports and providers

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { TASK_REPOSITORY_ADAPTER } from './application/di-map';
+import { TaskRepositoryTypeOrmAdapter } from './infra/task-repository.type-orm.adapter';
+import { CreateTaskHandler } from './application/command/create-task.handler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskOrmEntity } from './infra/orm/task.orm-entity';
+
+const taskRepository: Provider = {
+  provide: TASK_REPOSITORY_ADAPTER,
+  useClass: TaskRepositoryTypeOrmAdapter,
+};
 
 @Module({
-  imports: [CqrsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'tasks.db',
+    entities: [TaskOrmEntity],
+    synchronize: true,
+  }), TypeOrmModule.forFeature([TaskOrmEntity]), CqrsModule],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [CreateTaskHandler, taskRepository],
 })
-export class TaskModule {}
+export class TaskModule { }
+
