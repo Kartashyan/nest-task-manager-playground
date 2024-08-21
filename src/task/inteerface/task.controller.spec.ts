74887eb3930822ec07e4314f90eb3ac2f1df5e7f
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-21 22:46:08 +0200

    refactor: Move task controller file to interface directory

--- src/task/inteerface/task.controller.spec.ts
+++ src/task/inteerface/task.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
-import { TaskService } from './task.service';
 
 describe('TaskController', () => {
   let controller: TaskController;
@@ -8,7 +7,7 @@ describe('TaskController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
-      providers: [TaskService],
+      providers: [],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
--- src/task/inteerface/task.controller.ts
+++ src/task/inteerface/task.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
 import { CommandBus } from '@nestjs/cqrs';
-import { CreateTaskCommand } from './application/command/create-task.command';
+import { CreateTaskCommand } from '../application/command/create-task.command';
 
-@Controller('task')
+@Controller('tasks')
 export class TaskController {
   constructor(
     private readonly commandBus: CommandBus,
   ) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    this.commandBus.execute(new CreateTaskCommand(createTaskDto.name, createTaskDto.description));
+  async create(@Body() createTaskDto: CreateTaskDto) {
+   await this.commandBus.execute(new CreateTaskCommand(createTaskDto.name, createTaskDto.description));
+   return { message: 'Task created' };
   }
 
   @Get()
