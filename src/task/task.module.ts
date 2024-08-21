78fdcc8191e0be7d8748b70c3f683e642f28480b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-21 23:52:46 +0200

    refactor: Update TaskModule to use TaskQueryAdapter and TaskRepositoryTypeOrmAdapter

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,16 +1,29 @@
 import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TaskController } from './inteerface/task.controller';
-import { TASK_REPOSITORY_ADAPTER } from './application/di-map';
+import { TASK_QUERY_ADAPTER, TASK_REPOSITORY_ADAPTER } from './application/di-map';
 import { TaskRepositoryTypeOrmAdapter } from './infra/task-repository.type-orm.adapter';
 import { CreateTaskHandler } from './application/command/create-task.handler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskOrmEntity } from './infra/orm/task.orm-entity';
+import { TaskQueryAdapter } from './infra/task-query.type-orm.adapter';
+import { FindTasksHandler } from './application/query/find-tasks.query-handler';
 
-const taskRepository: Provider = {
-  provide: TASK_REPOSITORY_ADAPTER,
-  useClass: TaskRepositoryTypeOrmAdapter,
-};
+const commandHandlers: Provider[] = [CreateTaskHandler];
+const queryHandlers: Provider[] = [FindTasksHandler];
+
+const application: Provider[] = [...commandHandlers, ...queryHandlers];
+
+const infrastructure: Provider[] = [
+  {
+    provide: TASK_QUERY_ADAPTER,
+    useClass: TaskQueryAdapter
+  },
+  {
+    provide: TASK_REPOSITORY_ADAPTER,
+    useClass: TaskRepositoryTypeOrmAdapter,
+  }
+];
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -20,7 +33,7 @@ const taskRepository: Provider = {
     synchronize: true,
   }), TypeOrmModule.forFeature([TaskOrmEntity]), CqrsModule],
   controllers: [TaskController],
-  providers: [CreateTaskHandler, taskRepository],
+  providers: [...application, ...infrastructure],
 })
 export class TaskModule { }
 
