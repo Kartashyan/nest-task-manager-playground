0f0f35f6e9777169d1ea732b1e940abf2c6e0dc4
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-21 23:51:28 +0200

    refactor: Remove findAll method from TaskRepositoryPort

--- src/task/domain/task.repository.ts
+++ src/task/domain/task.repository.ts
@@ -2,6 +2,5 @@ import { Task } from "./task.entity";
 
 export interface TaskRepositoryPort {
     save(task: Task): Promise<void>;
-    findAll(): Promise<Task[]>;
     findById(id: string): Promise<Task | null>;
 }
