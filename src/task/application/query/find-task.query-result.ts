9e5c811d7c019ed67f3b70be586bec0fc74599a9
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-21 23:52:00 +0200

    refactor: Remove createdAt and updatedAt properties from FindTaskByIdResult class

--- src/task/application/query/find-task.query-result.ts
+++ src/task/application/query/find-task.query-result.ts
@@ -4,6 +4,4 @@ export class FindTaskByIdResult implements IQueryResult {
   readonly id: string;
   readonly title: string;
   readonly description: string;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
 }
