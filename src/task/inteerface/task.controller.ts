66f18e647d95bb341fc34fe38a458a99afc81bfd
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-24 00:09:46 +0200

    refactor: Update TaskController findTasks method to handle query parameters with FindTasksRequestQueryString

--- src/task/inteerface/task.controller.ts
+++ src/task/inteerface/task.controller.ts
@@ -1,33 +1,33 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CommandBus, IQueryResult, QueryBus } from '@nestjs/cqrs';
 import { CreateTaskCommand } from '../application/command/create-task.command';
 import { FindTasksQuery } from '../application/query/find-tasks.query';
-import { FindTaskssRequestQueryString } from './dto/find-tasks-request-query-string';
+import { FindTasksRequestQueryString } from './dto/find-tasks-request-query-string';
 
 @Controller('tasks')
 export class TaskController {
   constructor(
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
-  ) {}
+  ) { }
 
   @Post()
   async create(@Body() createTaskDto: CreateTaskDto) {
-   await this.commandBus.execute(new CreateTaskCommand(createTaskDto.name, createTaskDto.description));
-   return { message: 'Task created' };
+    await this.commandBus.execute(new CreateTaskCommand(createTaskDto.name, createTaskDto.description));
+    return { message: 'Task created' };
   }
 
   @Get()
-  async find(query: FindTaskssRequestQueryString) {
+  async findTasks(@Query() query: FindTasksRequestQueryString) {
     const tasksQuery = new FindTasksQuery(query);
     return await this.queryBus.execute(tasksQuery);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-
+    
   }
 
   @Patch(':id/complete-task')
