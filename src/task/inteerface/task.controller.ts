bc52e5d0485e014cee381963593d46abdad8ba01
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-25 20:23:54 +0200

    feat: Implement task controller endpoints for find, update and complete

--- src/task/inteerface/task.controller.ts
+++ src/task/inteerface/task.controller.ts
@@ -5,6 +5,9 @@ import { FindTasksQuery } from '../application/query/find-tasks.query';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
 import { FindTasksRequestQueryString } from './dto/find-tasks-request-query-string';
+import { CompleteTaskCommand } from '../application/command/complete-task.command';
+import { UpdateTaskCommand } from '../application/command/update-task.command';
+import { FindTaskByIdQuery } from '../application/query/find-task-by-id.query';
 
 @Controller('tasks')
 export class TaskController {
@@ -27,15 +30,16 @@ export class TaskController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    
+    return await this.queryBus.execute(new FindTaskByIdQuery(id));
   }
 
   @Patch(':id/complete-task')
   async completete(@Param('id') id: string) {
-
+    this.commandBus.execute(new CompleteTaskCommand(id));
   }
+
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-
+    this.commandBus.execute(new UpdateTaskCommand(id, updateTaskDto));
   }
 }
