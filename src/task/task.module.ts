b623b0a5a6ff691f09648910fb729bca0d70cbbd
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-28 22:30:11 +0200

    refactor: Update TaskModule imports and dependencies

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,15 +1,15 @@
 import { Module, Provider } from '@nestjs/common';
-import { TaskController } from './interface/task.controller';
-import { TASK_QUERY_ADAPTER, TASK_REPOSITORY_ADAPTER } from './application/di-map';
-import { TaskRepositoryTypeOrmAdapter } from './infra/task-repository.type-orm.adapter';
-import { CreateTaskHandler } from './application/command/create-task.handler';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompleteTaskHandler } from './application/command/complete-task.handler';
+import { CreateTaskHandler } from './application/command/create-task.handler';
+import { FindTaskByIdQueryHandler } from './application/query/find-task-by-id.query-handler';
+import { FindTasksHandler } from './application/query/find-tasks.query-handler';
+import { TaskQueryPort } from './application/query/task.query.port';
+import { TaskRepositoryPort } from './domain/task.repository';
 import { TaskOrmEntity } from './infra/orm/task.orm-entity';
 import { TaskQueryAdapter } from './infra/task-query.type-orm.adapter';
-import { FindTasksHandler } from './application/query/find-tasks.query-handler';
-import { CompleteTaskHandler } from './application/command/complete-task.handler';
-import { FindTaskByIdQueryHandler } from './application/query/find-task-by-id.query-handler';
-import { TaskQueryPort } from './application/query/task.query.port';
+import { TaskRepositoryTypeOrmAdapter } from './infra/task-repository.type-orm.adapter';
+import { TaskController } from './interface/task.controller';
 
 const commandHandlers: Provider[] = [CreateTaskHandler, CompleteTaskHandler];
 const queryHandlers: Provider[] = [FindTasksHandler, FindTaskByIdQueryHandler];
@@ -22,7 +22,7 @@ const infrastructure: Provider[] = [
     useClass: TaskQueryAdapter
   },
   {
-    provide: TASK_REPOSITORY_ADAPTER,
+    provide: TaskRepositoryPort,
     useClass: TaskRepositoryTypeOrmAdapter,
   }
 ];
